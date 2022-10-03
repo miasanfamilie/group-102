4569a21bd94378959778411873303f7a9907586e
miasanfamilie <papava.levani87@gmail.com>
2022-10-03 17:29:09 +0400

    first commit

---
+++ lesson1.py
@@ -0,0 +1,18 @@
+from operator import truediv
+
+
+print("Hello World")
+name = "levani"
+surname = "papava"
+age = 35
+height = 189.5
+is_lawyer = True
+
+
+print(name + " " + surname + " " + str(age) + " " + str(height) + " " + str(is_lawyer))
+
+print(type(age))
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(is_lawyer))
